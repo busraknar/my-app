66c75c8e783f63bbbdb1eb5640974864a1dc44b6
busraknar <b.ertas91@gmail.com>
2024-10-23 01:13:54 +0300

    logo-pattern added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@
 function App() {
   return (
     <div className="h-full w-full flex items-center justify-center">
-    instagram clone
+      <div className="w-[380px] h-[581px] relative bg-logo-pattern bg-[length:468.32px _634.15px] bg-[top_left_-46px]">
+        <div className="w-[250px] h-[538px] absolute top-0 right-0">
+        <img className="w-full h-full" src="https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot1.png"/>
+        </div>
+      </div>
     </div>
   );
 }
