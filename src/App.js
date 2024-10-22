0f6531cd36cd65375c338cbb58e96a36a6541197
busraknar <b.ertas91@gmail.com>
2024-10-23 01:38:02 +0300

    useRef, transparent used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import { useEffect, useRef } from "react";
 
 function App() {
+
+
+  const ref = useRef()
+  useEffect(()=>{
+    console.log(ref.current)
+
+  }, [ref])
+
+
   return (
     <div className="h-full w-full flex items-center justify-center">
       <div className="w-[380px] h-[581px] relative bg-logo-pattern bg-[length:468.32px _634.15px] bg-[top_left_-46px]">
-        <div className="w-[250px] h-[538px] absolute top-0 right-0">
-        <img className="w-full h-full" src="https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot1.png"/>
+        <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]" ref={ref}>
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity" src="screenshot1.png" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity" src="screenshot2.png" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity" src="screenshot3.png" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity" src="screenshot4.png" />
+
+
+
         </div>
       </div>
     </div>
